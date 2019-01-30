5a1a597385e6f69639ed9a93a0be9030784e948d
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-01-30 11:19:47 +0100

    Add timeConvert.js

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,20 +1,21 @@
 let os = require('os');
+let time = require('../modules/timeConvert');
 
 function getOSinfo() {
-    var type = os.type();
+    let type = os.type();
     if(type === 'Darwin') {
         type = 'OSX';
     } else if(type === 'Windows_NT') {
         type = 'Windows';
     }
-    var release = os.release();
-    var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
-    var userInfo = os.userInfo();
+    const release = os.release();
+    const cpu = os.cpus()[0].model;
+    const uptime = os.uptime();
+    const userInfo = os.userInfo();
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('Uptime: ~', time.print(uptime) + '       ' + uptime);
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
