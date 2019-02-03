2b978880d6fadf4399914a4d2133d179d2459be9
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-02-03 18:11:01 +0100

    Fix if

--- app/program.js
+++ app/program.js
@@ -3,21 +3,22 @@ const OSinfo = require('../modules/OSinfo');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function () {
     const input = process.stdin.read();
-    if (input !== null) {
-        const instruction = input.trim();
-        switch (instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-            case '/getOSinfo':
-                OSinfo.print();
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        };
+    if (!input) {
+        return;
     }
+    const instruction = input.trim();
+    switch (instruction) {
+        case '/exit':
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+            break;
+        case '/sayhello':
+            process.stdout.write('hello!\n');
+            break;
+        case '/getOSinfo':
+            OSinfo.print();
+            break;
+        default:
+            process.stderr.write('Wrong instruction!\n');
+    };
 });
